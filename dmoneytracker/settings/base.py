0d9e479118af6a904d0f9b646bbfa859f5b68289
hiteshmohite19 <hiteshmohite19nov@gmail.com>
2026-06-23 20:01:02 +0530

    updated docker file

--- dmoneytracker/settings/base.py
+++ dmoneytracker/settings/base.py
@@ -23,7 +23,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
 
 ROOT_URLCONF = "dmoneytracker.urls"
 
